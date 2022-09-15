6a62c6db87455fe3acfc4458021399c6df599fd3
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-15 16:44:50 +0430

    Implement databse context

---
+++ src/Services/Catalog/SAF.Catalog.Api/Persistence/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using SAF.Catalog.Api.Infrastructure.Configuration;
+using SAF.Catalog.Api.Models;
+
+namespace SAF.Catalog.Api.Persistence;
+
+internal class DatabaseContext : IDatabaseContext
+{
+	public DatabaseContext(IOptions<DatabaseSettings> databaseSettings)
+	{
+		var client = new MongoClient(databaseSettings.Value.ConnectionString);
+		var database = client.GetDatabase(databaseSettings.Value.Name);
+
+		Products = database.GetCollection<Product>(databaseSettings.Value.CollectionName);
+	}
+
+	public IMongoCollection<Product> Products { get; init; }
+}
---
+++ src/Services/Catalog/SAF.Catalog.Api/Persistence/IDatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+using SAF.Catalog.Api.Models;
+
+namespace SAF.Catalog.Api.Persistence;
+
+internal interface IDatabaseContext
+{
+	IMongoCollection<Product> Products { get; }
+}
