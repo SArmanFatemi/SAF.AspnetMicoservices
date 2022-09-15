d22c75d0fd422216339153d04584d4c40a7a7ea7
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-15 16:42:04 +0430

    Add database settings to appSettings.json

---
+++ src/Services/Catalog/SAF.Catalog.Api/Infrastructure/Configuration/ConfigurationServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SAF.Catalog.Api.Infrastructure.Configuration;
+
+internal static class ConfigurationServices
+{
+	public static void AddConfigurationServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		void registerConfiguration<T>()
+			where T : class
+		{
+			services.Configure<T>(configuration.GetSection(typeof(T).Name));
+		}
+
+		registerConfiguration<DatabaseSettings>();
+	}
+}
---
+++ src/Services/Catalog/SAF.Catalog.Api/Infrastructure/Configuration/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SAF.Catalog.Api.Infrastructure.Configuration;
+
+public class DatabaseSettings
+{
+	public string? ConnectionString { get; init; }
+
+	public string? Name { get; init; }
+
+	public string? CollectionName { get; init; }
+}
