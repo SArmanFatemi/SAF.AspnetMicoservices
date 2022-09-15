4a8e6c3c993925764949502cb5a8eaf110a60d21
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-15 15:55:31 +0430

    Add product model

---
+++ src/Services/Catalog/SAF.Catalog.Api/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace SAF.Catalog.Api.Models;
+
+// TODO: Is there any chance for using fluent configuration
+// TODO: Resolve null problem
+public class Product
+{
+	[BsonId]
+	[BsonRepresentation(BsonType.ObjectId)]
+	public string Id { get; set; }
+
+	public string Name { get; set; }
+
+	public string Category { get; set; }
+
+	public string Summary { get; set; }
+
+	public string Description { get; set; }
+
+	public string ImageFile { get; set; }
+
+	public decimal Price { get; set; }
+}
