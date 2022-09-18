1879b2711da5a8948d76d3b559db1eea5ba7ea0b
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-18 11:47:16 +0430

    Add shopping cart and shopping cart item related models

---
+++ src/Services/Basket/SAF.Basket.Api/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SAF.Basket.Api.Models;
+
+public class ShoppingCart
+{
+	[JsonConstructor]
+	public ShoppingCart(string username, List<ShoppingCartItem> items)
+	{
+		Username = username;
+		Items = items;
+	}
+
+	public ShoppingCart(string username) : this(username, new List<ShoppingCartItem>())
+	{
+	}
+
+	public string Username { get; set; }
+
+	public List<ShoppingCartItem> Items { get; set; }
+
+	public decimal TotalPrice => Items.Sum(c => c.Price * c.Quantity);
+}
