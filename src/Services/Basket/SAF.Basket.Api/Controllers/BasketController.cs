d6142edfdc72acdb70692e80b52ee9073f3e291c
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-19 12:16:43 +0430

    Use file-scope namespace

--- src/Services/Basket/SAF.Basket.Api/Controllers/BasketController.cs
+++ src/Services/Basket/SAF.Basket.Api/Controllers/BasketController.cs
@@ -3,36 +3,35 @@ using SAF.Basket.Api.Models;
 using SAF.Basket.Api.Repositories;
 using System.Net;
 
-namespace SAF.Basket.Api.Controllers
+namespace SAF.Basket.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
 {
-	[Route("api/v1/[controller]")]
-	[ApiController]
-	public class BasketController : ControllerBase
+	private readonly IShoppingCartRepository shoppingCartRepository;
+
+	public BasketController(IShoppingCartRepository shoppingCartRepository)
 	{
-		private readonly IShoppingCartRepository shoppingCartRepository;
-
-		public BasketController(IShoppingCartRepository shoppingCartRepository)
-		{
-			this.shoppingCartRepository = shoppingCartRepository;
-		}
-
-		[HttpGet("{username}")]
-		[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
-		public async Task<ActionResult<ShoppingCart>> Get(string username, CancellationToken cancellationToken)
-		{
-			var shoppingCart = await shoppingCartRepository.Get(username, cancellationToken);
-			return base.Ok(shoppingCart ?? new ShoppingCart(username));
-		}
-
-		[HttpPost]
-		[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
-		public async Task<ActionResult<ShoppingCart>> Update([FromBody] ShoppingCart shoppingCart, CancellationToken cancellationToken)
-		{
-			return Ok(await shoppingCartRepository.Update(shoppingCart, cancellationToken));
-		}
-
-		[HttpDelete("{username}")]
-		public async Task Delete(string username, CancellationToken cancellationToken) =>
-			await shoppingCartRepository.Delete(username, cancellationToken);
+		this.shoppingCartRepository = shoppingCartRepository;
 	}
+
+	[HttpGet("{username}")]
+	[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+	public async Task<ActionResult<ShoppingCart>> Get(string username, CancellationToken cancellationToken)
+	{
+		var shoppingCart = await shoppingCartRepository.Get(username, cancellationToken);
+		return base.Ok(shoppingCart ?? new ShoppingCart(username));
+	}
+
+	[HttpPost]
+	[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+	public async Task<ActionResult<ShoppingCart>> Update([FromBody] ShoppingCart shoppingCart, CancellationToken cancellationToken)
+	{
+		return Ok(await shoppingCartRepository.Update(shoppingCart, cancellationToken));
+	}
+
+	[HttpDelete("{username}")]
+	public async Task Delete(string username, CancellationToken cancellationToken) =>
+		await shoppingCartRepository.Delete(username, cancellationToken);
 }
