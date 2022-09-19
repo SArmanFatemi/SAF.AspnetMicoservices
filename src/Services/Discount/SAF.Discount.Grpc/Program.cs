bbaecc8c38a6752ac5a1d3540c60f739b81f85dc
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-19 12:51:38 +0430

    Remove default proto files

--- src/Services/Discount/SAF.Discount.Grpc/Program.cs
+++ src/Services/Discount/SAF.Discount.Grpc/Program.cs
@@ -1,6 +1,5 @@
 using SAF.Discount.Grpc.Infrastructure.Configuration;
 using SAF.Discount.Grpc.Repositories;
-using SAF.Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +16,6 @@ var app = builder.Build();
 app.UseDatabaseMigration();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
