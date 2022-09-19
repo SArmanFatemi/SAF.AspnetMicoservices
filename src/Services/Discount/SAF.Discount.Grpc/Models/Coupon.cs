5c5a7efd1ba2f9b856c5d932eb96f164079f188b
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-19 12:14:09 +0430

    Add coupon model to grpc project

---
+++ src/Services/Discount/SAF.Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,20 @@
+﻿namespace SAF.Discount.Grpc.Models;
+
+public class Coupon
+{
+	public Coupon(int id, string productName, string description, int amount)
+	{
+		Id = id;
+		ProductName = productName;
+		Description = description;
+		Amount = amount;
+	}
+
+	public int Id { get; set; }
+
+	public string ProductName { get; set; }
+
+	public string Description { get; set; }
+
+	public int Amount { get; set; }
+}
