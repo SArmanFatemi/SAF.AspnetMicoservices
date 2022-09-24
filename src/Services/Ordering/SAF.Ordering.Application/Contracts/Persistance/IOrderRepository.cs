95862eecb40ded59bf6cb35043fe474864fdc601
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-24 15:57:15 +0330

    Implement persistances contracts

---
+++ src/Services/Ordering/SAF.Ordering.Application/Contracts/Persistance/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using SAF.Ordering.Domain.Entities;
+
+namespace SAF.Ordering.Application.Contracts.Persistance;
+
+public interface IOrderRepository : IAsyncRepository<Order>
+{
+	Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+}
