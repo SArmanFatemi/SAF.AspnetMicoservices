df6ed4e1847a135217ea7842a259d164012bae39
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-24 15:56:38 +0330

    Add email services and models

---
+++ src/Services/Ordering/SAF.Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using SAF.Ordering.Application.Models;
+
+namespace SAF.Ordering.Application.Contracts.Infrastructure;
+
+public interface IEmailService
+{
+	Task<bool> SendEmail(Email email);
+}
---
+++ src/Services/Ordering/SAF.Ordering.Application/Models/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAF.Ordering.Application.Models;
+
+public class Email
+{
+	// TODO: Resolve null problem
+	public string To { get; set; }
+
+	public string Subject { get; set; }
+
+	public string Body { get; set; }
+}
