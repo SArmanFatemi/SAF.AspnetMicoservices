689c6628d931e35a91cabd6d03b82297d64807cd
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-24 14:08:08 +0330

    Impelement ordering microservice domain

---
+++ src/Services/Ordering/SAF.Ordering.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace SAF.Ordering.Domain.Common;
+
+public abstract class BaseEntity
+{
+	// TODO: Resolve null problem
+	public int Id { get; protected set; }
+
+	public string CreateBy { get; set; }
+
+	public DateTime CreateDateTime { get; set; }
+
+	public string LastModifyBy { get; set; }
+
+	public DateTime LastModificationDateTime { get; set; }
+}
---
+++ src/Services/Ordering/SAF.Ordering.Domain/Common/ValueObject.cs
@@ -0,0 +1,39 @@
+﻿namespace SAF.Ordering.Domain.Common;
+
+internal abstract class ValueObject
+{
+	protected static bool EqualOperator(ValueObject left, ValueObject right)
+	{
+		if (left is null ^ right is null)
+		{
+			return false;
+		}
+
+		return left?.Equals(right) is not false;
+	}
+
+	protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+	{
+		return EqualOperator(left, right) is false;
+	}
+
+	protected abstract IEnumerable<object> GetEqualityComponents();
+
+	public override bool Equals(object? obj)
+	{
+		if (obj is null || obj.GetType() != GetType())
+		{
+			return false;
+		}
+
+		var other = (ValueObject)obj;
+		return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+	}
+
+	public override int GetHashCode()
+	{
+		return GetEqualityComponents()
+			.Select(x => x != null ? x.GetHashCode() : 0)
+			.Aggregate((x, y) => x ^ y);
+	}
+}
