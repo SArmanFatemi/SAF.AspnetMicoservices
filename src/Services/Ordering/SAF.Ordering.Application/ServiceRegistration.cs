1868b8ca34fa2ec56d542181f3d9ebb2395d8a0d
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-25 14:20:54 +0330

    Implement service registration

---
+++ src/Services/Ordering/SAF.Ordering.Application/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SAF.Ordering.Application.Behaviors;
+using System.Reflection;
+
+namespace SAF.Ordering.Application;
+
+public static class ServiceRegistration
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddAutoMapper(Assembly.GetExecutingAssembly());
+		services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+		services.AddMediatR(Assembly.GetExecutingAssembly());
+
+		// Behviours
+		services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+		services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+		return services;
+	}
+}
