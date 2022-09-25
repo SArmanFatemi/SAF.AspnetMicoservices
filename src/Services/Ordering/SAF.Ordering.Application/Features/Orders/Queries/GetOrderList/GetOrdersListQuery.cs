342c71b6750d172e927c0b7d2868320e5512394f
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-25 15:23:40 +0330

    Rename order queries

--- src/Services/Ordering/SAF.Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQuery.cs
+++ src/Services/Ordering/SAF.Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQuery.cs
@@ -2,11 +2,11 @@
 
 namespace SAF.Ordering.Application.Features.Orders.Queries.GetOrderList;
 
-public class GetOrderRequestQuery : IRequest<List<OrderViewModel>>
+public class GetOrdersListQuery : IRequest<List<OrderViewModel>>
 {
 	public string UserName { get; set; }
 
-	public GetOrderRequestQuery(string userName)
+	public GetOrdersListQuery(string userName)
 	{
 		UserName = userName;
 	}
--- src/Services/Ordering/SAF.Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
+++ src/Services/Ordering/SAF.Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -4,18 +4,18 @@ using SAF.Ordering.Application.Contracts.Persistance;
 
 namespace SAF.Ordering.Application.Features.Orders.Queries.GetOrderList;
 
-public class GetOrderListQueryHandler : IRequestHandler<GetOrderRequestQuery, List<OrderViewModel>>
+public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrderViewModel>>
 {
 	private readonly IOrderRepository orderRepository;
 	private readonly IMapper mapper;
 
-	public GetOrderListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+	public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
 	{
 		this.orderRepository = orderRepository;
 		this.mapper = mapper;
 	}
 
-	public async Task<List<OrderViewModel>> Handle(GetOrderRequestQuery request, CancellationToken cancellationToken)
+	public async Task<List<OrderViewModel>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
 	{
 		var result = await orderRepository.GetOrdersByUserName(request.UserName);
 		return mapper.Map<List<OrderViewModel>>(result);
