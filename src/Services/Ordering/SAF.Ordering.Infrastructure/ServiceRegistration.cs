909d117a774b9e8485f2ab73fdbe213bacdd4d85
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-25 16:19:33 +0330

    Develop infrastructure service registration

---
+++ src/Services/Ordering/SAF.Ordering.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SAF.Ordering.Application.Contracts.Infrastructure;
+using SAF.Ordering.Application.Contracts.Persistance;
+using SAF.Ordering.Application.Models;
+using SAF.Ordering.Infrastructure.Persistence;
+using SAF.Ordering.Infrastructure.Repositories;
+
+namespace SAF.Ordering.Infrastructure;
+
+public static class ServiceRegistration
+{
+	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDbContext<DatabaseContext>(options =>
+			options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+		services.AddScoped(typeof(IAsyncRepository<>), typeof(AsyncRepository<>));
+		services.AddScoped<IOrderRepository, OrderRepository>();
+
+		services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+		services.AddTransient<IEmailService, EmailService>();
+
+		return services;
+	}
+}
