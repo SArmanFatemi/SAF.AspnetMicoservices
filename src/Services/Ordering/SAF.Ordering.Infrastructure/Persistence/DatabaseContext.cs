eb7f633330794fcd634a7ee462842515adcd1104
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-25 15:49:30 +0330

    Implement database context

---
+++ src/Services/Ordering/SAF.Ordering.Infrastructure/Persistence/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SAF.Ordering.Domain.Common;
+using SAF.Ordering.Domain.Entities;
+
+namespace SAF.Ordering.Infrastructure.Persistence;
+
+public class DatabaseContext : DbContext
+{
+	// TODO: Resolve null problems
+	public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Order> Orders { get; set; }
+
+	public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+	{
+		foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+		{
+			switch (entry.State)
+			{
+				case EntityState.Added:
+					entry.Entity.CreateDateTime = DateTime.Now;
+					entry.Entity.CreateBy = "swn";
+					break;
+				case EntityState.Modified:
+					entry.Entity.LastModificationDateTime = DateTime.Now;
+					entry.Entity.LastModifyBy = "swn";
+					break;
+			}
+		}
+
+		return base.SaveChangesAsync(cancellationToken);
+	}
+}
---
+++ src/Services/Ordering/SAF.Ordering.Infrastructure/Persistence/DatabaseContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using SAF.Ordering.Domain.Entities;
+
+namespace SAF.Ordering.Infrastructure.Persistence
+{
+	internal static class DatabaseContextSeed
+	{
+		public static async Task SeedAsync(DatabaseContext databaseContext, ILogger<DatabaseContext> logger)
+		{
+			if (databaseContext.Orders.Any() is false)
+			{
+				await databaseContext.Orders.AddRangeAsync(GetPreconfiguredOrders());
+				await databaseContext.SaveChangesAsync();
+
+				logger.LogInformation("Seed database associated with context {DbContextName}", typeof(DatabaseContext).Name);
+			}
+		}
+
+		private static IEnumerable<Order> GetPreconfiguredOrders()
+		{
+			return new List<Order>
+			{
+				new ()
+				{
+					UserName = "swn",
+					FirstName = "Seyed Arman",
+					LastName = "Fatemi",
+					EmailAddress = "s.arman.fatemi@gmail.com",
+					AddressLine = "Tehran",
+					Country = "Iran",
+					TotalPrice = 350
+			}
+			};
+		}
+	}
+}
