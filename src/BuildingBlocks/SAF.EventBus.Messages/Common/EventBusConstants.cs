bfebb06281fa27aae94f394e26b70790f00e0ea7
Seyed Arman Fatemi <S.Arman.Fatemi@outlook.com>
2022-09-29 17:58:27 +0330

    Implement basket checkout event

---
+++ src/BuildingBlocks/SAF.EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace SAF.EventBus.Messages.Common;
+
+public static class EventBusConstants
+{
+	public const string BasketCheckoutQueue = "basket_checkout_queue";
+}
---
+++ src/BuildingBlocks/SAF.EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace SAF.EventBus.Messages.Events;
+
+public class IntegrationBaseEvent
+{
+	public IntegrationBaseEvent()
+	{
+		Id = Guid.NewGuid();
+		CreationDateTime = DateTime.Now;
+	}
+
+	public IntegrationBaseEvent(Guid id, DateTime creationDateTime)
+	{
+		Id = id;
+		CreationDateTime = creationDateTime;
+	}
+
+	public Guid Id { get; private set; }
+
+	public DateTime CreationDateTime { get; set; }
+}
